68f0a200321daab70a72f99c62a34ac6464cc1f0
Kanta Oikawa <k.oikawa@kantacky.com>
2023-02-01 21:44:16 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Kanta Oikawa on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
