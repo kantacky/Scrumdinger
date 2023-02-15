ab83a1532e6c82e27a03b18fa6237aef46443df6
Kanta Oikawa <k.oikawa@kantacky.com>
2023-02-16 08:41:06 +0900

    [Update] Finish the SwiftUI part iOS App Dev Tutorial

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -1,17 +1,35 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by Kanta Oikawa on 2023/02/01.
-//
-
 import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ScrumsView(scrums: $store.scrums) {
+                    Task {
+                        do {
+                            let _ = try await ScrumStore.save(scrums: store.scrums)
+                        } catch {
+                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
+                        }
+                    }
+                }
+            }
+            .task {
+                do {
+                    store.scrums = try await ScrumStore.load()
+                } catch {
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
+                }
+            }
+            .sheet(item: $errorWrapper, onDismiss: {
+                store.scrums = DailyScrum.sampleData
+            }) { wrapper in
+                ErrorView(errorWrapper: wrapper)
+            }
         }
     }
 }
